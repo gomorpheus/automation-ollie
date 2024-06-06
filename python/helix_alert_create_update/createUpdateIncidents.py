55e509568820cb0b6a8191f5a7bd5f1584296080
Ollie Phillips <ollie@interject.io>
2024-06-06 15:17:15 +0100

    we have the stateCache working and the logic to support the new closed incident comparison

--- python/helix_alert_create_update/createUpdateIncidents.py
+++ python/helix_alert_create_update/createUpdateIncidents.py
@@ -32,15 +32,10 @@ helixBaseUrl = "https://nexio-dev-restapi.onbmc.com"
 morpheusToken = "Bearer %s" %(morpheus["morpheus"]["apiAccessToken"])
 morpheusBaseUrl = morpheus["morpheus"]["applianceHost"]
 
-##alertsStateCacheFile = "/var/opt/morpheus/morpheus-ui/caches/incidentStateCache.json"
-incidentStateCacheFile = "./incidentStateCache1.json"
+##incidentStateCacheFile = "/var/opt/morpheus/morpheus-ui/caches/incidentStateCache.json"
+incidentStateCacheFile = "./incidentStateCache.json"
 incidentState = []
-incidentStateAvailable = False
-
-## TODO review use of these globals
-newAlerts = False
-updatedAlerts = False
-pollIntervalSecs = 300
+firstPass = False
 
 ## gives us the time since last poll
 ## in format required for lastUpdated in query
@@ -61,29 +56,30 @@ def getSince(pollInt):
 ## we use this to create the first state file against which we can start to compare
 ## subsequent calls, each time updating the state with the latest results
 def preflight():
-    global incidentStateAvailable
     global incidentState
     global morpheusBaseUrl
     global morpheusToken
+    global firstPass
+
+    if debug:
+        print("INFO: script is running in debug mode, expect verbose logging")
 
     msg = "\nuser: %s\npassword: %s\n" % (helixUser, helixPassword)
     debugP(msg)
 
     ## check for cache, if exists load it
     if exists(incidentStateCacheFile):
-        incidentStateAvailable = True
         f = open(incidentStateCacheFile, "r")
         state = f.read()
         incidentState = json.loads(state)
         print("INFO: cached incidents have been read from disk")
-        msg = "incidentStateAvailable: %s" % incidentStateAvailable
-        debugP(msg)
         debugP("incidents from state:")
         debugP(incidentState)
     else:
         ## if not exist make an initial api call and use this as state
         incidents = pollIncidents(morpheusBaseUrl, morpheusToken)
         incidentState = incidents
+        firstPass = True
         debugP("incidents from initial pull:")
         debugP(incidentState)
 
@@ -141,40 +137,130 @@ def debugP(message):
     if debug:
         print("DEBUG:", message)
 
+
 ## get incidents from morpheus with API
 def pollIncidents(baseURL, token):
     ##lastUpdated = getSince(pollIntervalSecs)
-    incidentsAPI = "https://%s/api/monitoring/incidents?max=1000&offset=0&Status=open" % (baseURL)
+    incidentsAPI = "https://%s/api/monitoring/incidents?max=1000&offset=0&status=open" % (baseURL)
     headers = {
         "accept": "application/json",
         "authorization": token
     }
     res = requests.get(incidentsAPI, headers=headers, verify=False)
     incidents = res.json()["incidents"]
-    debugP("%s incident(s) found" % len(incidents))
+    debugP("API response: %s incident(s) found" % len(incidents))
     debugP(incidents)
     return incidents
 
+def writeIncidentStateFile(incidents):
+    global incidentStateCacheFile
+    f = open(incidentStateCacheFile, "w")
+    f.write(json.dumps(incidents))
+    f.close()
+    print("INFO: incidents cached for tracking in file: %s" % incidentStateCacheFile)
+
 
 def main():
+    newIncidents = []
+    closedIncidents = []
+
     ## pre-flight, prints some debug, checks for existence of cache and if exists loads it
+    ## if not exist makes an initial call to Morpheus API to create baseline state
+    ## which should only be needed on first run
     preflight()
 
     ## poll morpheus incidents
     morpheusIncidents = pollIncidents(morpheusBaseUrl, morpheusToken)
 
+    ## TODO refactor to functions at some point
+
     ## we have state and we have polled API, we need to compare JSON objects
     ## if in API call and not state file it is new incident
+    print("INFO: checking for new incidents")
+    for mInc in morpheusIncidents:
+        foundInState = False
+        for sInc in incidentState:
+            if mInc["id"] == sInc["id"]:
+                foundInState = True
+
+        if foundInState == False:
+            ## new incident
+            newIncidents.append(mInc)
+
+    print("INFO: new incidents: %s" %(len(newIncidents)))
+
     ## if in state file and not the API call it is closed incident (we only poll for open incidents)
-    ## make this calculations, fill two lists, if either list > 0 we need to open a Helix session
-    ## and make the appropriate API calls by interating through both lists
+    print("INFO: checking for closed incidents")
+    for sInc in incidentState:
+        foundInAPI = False
+        for mInc in morpheusIncidents:
+            if sInc["id"] == mInc["id"]:
+                foundInAPI = True
+                ## capture existig helix IDs in state including dummy
+                if not firstPass:
+                    mInc["helixID"] = sInc["helixID"]
 
-    ##headers = authenticate(helixUser, helixPassword)
+        if foundInAPI == False:
+            ## closed incident
+            closedIncidents.append(sInc)
 
-    ## if we opened a session we need to logout before exit
-    ##logout(headers)
 
-    ## we need to update the state file
+
+    print("INFO: closed incidents: %s" %(len(closedIncidents)))
+
+    if (len(closedIncidents) > 0 or len(newIncidents) > 0):
+        ## we have changes, we need a session on helix
+        print("INFO: there are updates needed on helix")
+        headers = authenticate(helixUser, helixPassword)
+
+        if len(newIncidents) > 0:
+            ## add new
+            ## we will track the array element and use to add the helix incident ref
+
+            for nInc in newIncidents:
+                debugP("newIncidents %s, incident %s" % (newIncidents, nInc['id']))
+
+                ## call to helix to create incident
+                debugP("creating incident: %s in helix" % cInc["id"])
+
+                ## we need to add the helix ID
+                for mInc in morpheusIncidents:
+                    if nInc["id"] == mInc["id"]:
+                        mInc["helixID"] = "new"
+
+
+        if len(closedIncidents) > 0:
+            ## close the open incidents
+
+            for cInc in closedIncidents:
+                ## some incidents formed initial state and were not created in helix
+                ## ignore all closed incidents with helixID of "dummy"
+                if cInc["helixID"] != "dummy":
+                    ## call to helix to update the incident to closed
+                    debugP("closing morpheus incident id: %s, helix id: %s in helix" % (cInc["id"], cInc["helixID"]))
+
+            print("")
+        ## if we opened a session we need to logout before exit
+
+
+        logout(headers)
+    else:
+        print("INFO: there are no updates to make on helix")
+
+
+    ## we need to update the state file even if no changes, as we haven't written it yet
+    ## if this is a first pass we need to add a dummy helix id, so we can ignore these and not attempt to close them
+    ## because we will not have created them in Helix
+    if firstPass:
+        debugP("first pass adding dummy helixID")
+        dummyCounter = 0
+        for mInc in morpheusIncidents:
+            morpheusIncidents[dummyCounter]["helixID"] = "dummy"
+            dummyCounter += 1
+
+    writeIncidentStateFile(morpheusIncidents)
+
+    print("INFO: script execution complete")
 
 
 ## start main execution
